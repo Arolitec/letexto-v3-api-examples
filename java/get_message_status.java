7b538b98c1a878786e7c705326cdca30e64f1362
AbrahamAkreiry <akreiryabraham@arolitec.com>
2023-11-08 11:49:26 +0000

    LT-960 feat(get_message_status.java): Add Java get message status example code

---
+++ java/get_message_status.java
@@ -0,0 +1,19 @@
+import okhttp3.*;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
+
+        Request request = new Request.Builder()
+                .url("<REPLACE_WITH_PROD_URL>/v1/message/<YOUR MESSAGE ID>/status")
+                .method("GET", null)
+                .addHeader("Authorization", "Bearer <YOUR_TOKEN>")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        String responseBody = response.body().string();
+        System.out.println(responseBody);
+    }
+}
