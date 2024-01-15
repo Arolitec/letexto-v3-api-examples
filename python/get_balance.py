4ac7f0581ec6898700ed6862e62bc1ebd8c468b8
Martial Anouman <80514220+martialanouman@users.noreply.github.com>
2024-01-15 15:45:26 +0000

    fix: remove reference to undeclared param

--- python/get_balance.py
+++ python/get_balance.py
@@ -2,6 +2,6 @@ import requests
 
 url = "<REPLACE_WITH_PROD_URL>/v1/users/balance?token=<REPLACE_WITH_YOUR_MESSAGE_ID>"
 
-response = requests.get(url, headers=headers)
+response = requests.get(url)
 data = response.content.decode("utf-8")
 print(data)
